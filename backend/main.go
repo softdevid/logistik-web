c8dfb3246d8f55a306f801b384401aa76ecbaf7d
Ardiputra614 <ardianto8306@gmail.com>
2026-07-24 21:54:12 +0700

    feat: add CORS middleware configuration for API

--- backend/main.go
+++ backend/main.go
@@ -4,14 +4,28 @@ import (
 	"backend/config"
 	"backend/routes"
 
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// konek database dulu sebelum setup routes
 	config.ConnectDatabase()
-
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:3000",
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept", "x-api-key"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	routes.SetUpRoutes(r)
 	r.Run() // default listen di :8080, ini harus paling akhir
+	// config.DB.AutoMigrate()
 }
